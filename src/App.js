3999e3248ebd703d24c693868b34a71d8431ebcd
syafiqinsanp <112202006504@mhs.dinus.ac.id>
2023-07-04 11:27:43 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Login from './components/login';
 import './App.css';
 import Register from './components/Homepage/Home';
+import Layout from './components/pages/layout';
 
 function App() {
   return (
     <div className="App">
-      <Register/>
+      <Login/>
     </div>
   );
 }
